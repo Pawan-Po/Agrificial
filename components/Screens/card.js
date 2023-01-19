9b4025f319ad54059ef3187a23b79d1749a085f8
PrernaChhetri <110452388+PrernaChhetri@users.noreply.github.com>
2023-01-19 12:00:33 +0600

    Home Screen

---
+++ components/Screens/card.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
+
+const Card = (props) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Text style={styles.cardTitle}>{props.title}</Text>
+      <Text style={styles.cardDescription}>{props.description}</Text>
+      <Image source={props.image} style={styles.cardImage} resizeMode="contain" />
+      {/* <TouchableOpacity style={styles.cardButton} onPress={props.onPress}>
+        <Text style={styles.cardButtonText}>{props.buttonText}</Text>
+      </TouchableOpacity> */}
+      
+    </View>
+  );
+};
+
+const styles = {
+  cardContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    backgroundColor: '#212121',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 50,
+    width: 350,
+    alignSelf: 'center',
+    
+  },
+  cardImage: {
+    width: '100%',
+    height: '100%',
+    flex: 0.5,
+    //flexDirection: 'row',
+    padding:40,
+    marginTop: -40,
+    marginLeft: 200, 
+    alignSelf: 'center',
+    margin: 5,
+    borderRadius: 5,   
+  },
+  cardTitle: {
+    fontSize: 18,
+    margin: 5,
+    color: '#fff',
+  },
+  cardDescription: {
+    fontSize: 14,
+    margin: 5,
+    color: '#fff',
+    paddingTop: 10, 
+  },
+  cardButton: {
+    backgroundColor: '#0288D1',
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: 'flex-end',
+    margin: 5,
+  },
+  cardButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+};
+
+export default Card;
+
--- components/Screens/landingpage.js
+++ components/Screens/landingpage.js
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import { View, Text, ImageBackground,StyleSheet, } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 import Dorji from "../../assets/Dorji.png";
 import crop_recommendations from "../../assets/cropRecommendations.png";
 import plant_disease_detection from "../../assets/plant-disease-detection.png";
 import tips_and_FAQ from "../../assets/tips-and-FAQ.png";
 import Card from './card';
 
-export default function HomeScreen({ navigation }) {
+
+
+
+
+export default function Dashboard(){
+    
     return(
         <ImageBackground source={Dorji} style={styles.container}>
                 <Card
@@ -33,43 +38,7 @@ export default function HomeScreen({ navigation }) {
             
        </ImageBackground>  
     );
-}
-// import React from 'react';
-// import {StyleSheet, Text, View} from 'react-native';
-// import PieChart from 'react-native-pie-chart';
-
-// const HomeScreen = () => {
-//   const widthAndHeight = 250;
-//   const series = [123, 321, 123, 789, 537];
-//   const sliceColor = ['#F44336', '#2196F3', '#FFEB3B', '#4CAFr50', '#FF9800'];
-//   return (
-//     <View>
-//         <Text style={styles.title}>Doughnut</Text>
-//         <PieChart
-//           widthAndHeight={widthAndHeight}
-//           series={series}
-//           sliceColor={sliceColor}
-//           doughnut={true}
-//           coverRadius={0.45}
-//           coverFill={'#FFF'}
-//         />
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   scrollContainer: {
-//     flex: 1,
-//   },
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//   },
-//   title: {
-//     fontSize: 24,
-//     margin: 10,
-//   },
-// });
+} 
 
 const styles = StyleSheet.create({
     container: {
@@ -86,4 +55,6 @@ const styles = StyleSheet.create({
     
 });
 
-// export default HomeScreen;
+
+
+
