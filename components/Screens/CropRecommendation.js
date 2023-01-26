e0ede4b961b003c912aa440f3db316184cba39f1
PravaatChhetri <pravaatchhetri66@gmail.com>
2023-01-26 15:32:54 +0600

    updated contacts

--- components/Screens/CropRecommendation.js
+++ components/Screens/CropRecommendation.js
@@ -4,42 +4,39 @@ import Dorji from "../../assets/Dorji.png";
 import { View, Text, ScrollView } from "react-native";
 
 const data = [
-  { date: "3 Jan , 2023", veggies: ["Potato", "Chilli", "Cauliflower"] },
+  { date: "3 Jan , 2023", dataSet: "Crop Recommendation" },
   ,
-  { date: "1 Jan , 2023", veggies: ["Potato", "Tomato", "Cabbage"] },
+  { date: "1 Jan , 2023", dataSet: "Crop Recommendation" },
 ];
-const CardView = ({ date, veggies }) => {
+const CardView = ({ date, dataSet }) => {
   return (
     <View
       style={{
-        backgroundColor: "#111111",
+        backgroundColor: "#649068",
         color: "white",
         borderRadius: 20,
         padding: 10,
         marginBottom: 10,
-        opacity:0.8
+        opacity: 0.8,
       }}
     >
       <Text style={{ color: "white", fontSize: 20, textAlign: "center" }}>
         {date}
       </Text>
-      {veggies.map((item, index) => {
-        return (
-          <Text
-            style={{
-              padding: 5,
-              margin: 3,
-              fontSize: 20,
-              textAlign: "center",
-              borderRadius: 10,
-              backgroundColor: "white",
-            }}
-            key={index}
-          >
-            {item}
-          </Text>
-        );
-      })}
+      <View
+        style={{
+          padding: 5,
+          margin: 3,
+          minHeight: 200,
+          borderRadius: 10,
+          backgroundColor: "white",
+        }}
+      >
+        <Text style={{ fontSize: 20, fontWeight: "bold",textAlign:'center' }}>
+          Crop Recommendation
+        </Text>
+        <Text style={{ fontSize: 14, fontWeight: '200',margin:5 }}>{dataSet}</Text>
+      </View>
     </View>
   );
 };
@@ -49,11 +46,13 @@ const CropRecommendation = () => {
       source={Dorji}
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text style={{marginTop:20,fontSize:20,fontWeight:'bold' }}>Crop Recommended</Text>
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "bold" }}>
+        Crop Recommended
+      </Text>
       <ScrollView style={{ marginTop: 20, width: 350 }}>
         {data.map((item, index) => {
           return (
-            <CardView key={index} date={item.date} veggies={item.veggies} />
+            <CardView key={index} date={item.date} dataSet={item.dataSet} />
           );
         })}
       </ScrollView>
--- components/Screens/contact.js
+++ components/Screens/contact.js
@@ -1,44 +1,57 @@
 import React from "react";
 import { ImageBackground } from "react-native";
 import Dorji from "../../assets/Dorji.png";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 
 const data = [
-  { company: "DHI", contact_details: ["00975-02-336257", "336258", "dhi@dhi.bt"] },
+  {
+    company: require("../../assets/dhiLogo.png"),
+    contact_details: [
+      "+975-02-336257",
+      "ThimphuTechPark, Thimphu, Bhutan",
+      "dhi@dhi.bt",
+    ],
+  },
   ,
 ];
 const CardView = ({ company, contact_details }) => {
   return (
     <View
       style={{
-        backgroundColor: "#111111",
+        backgroundColor: "#649068",
         color: "white",
         borderRadius: 20,
         padding: 10,
         marginBottom: 10,
-        opacity:0.8
+        opacity: 0.8,
       }}
     >
-      <Text style={{ color: "white", fontSize: 20, textAlign: "center" }}>
-        {company}
-      </Text>
-      {contact_details.map((item, index) => {
-        return (
-          <Text
-            style={{
-              padding: 5,
-              margin: 3,
-              fontSize: 20,
-              textAlign: "center",
-              borderRadius: 10,
-              backgroundColor: "white",
-            }}
-            key={index}
-          >
-            {item}
-          </Text>
-        );
-      })}
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Image source={company} style={{ height: 150, width: 150 }} />
+      </View>
+      <View
+        style={{
+          paddingVertical: 25,
+          paddingHorizontal: 25,
+
+          margin: 3,
+          borderRadius: 10,
+          backgroundColor: "white",
+        }}
+      >
+        <Text style={{ fontSize: 18, marginBottom: 5, fontWeight: "500" }}>
+          Contact No :{" "}
+          <Text style={{ fontWeight: "300" }}>{contact_details[0]}</Text>
+        </Text>
+        <Text style={{ fontSize: 18, marginBottom: 5, fontWeight: "500" }}>
+          Address :{" "}
+          <Text style={{ fontWeight: "300", textAlign:'right' }}>{contact_details[1]}</Text>
+        </Text>
+        <Text style={{ fontSize: 18, marginBottom: 5, fontWeight: "500" }}>
+          email :{" "}
+          <Text style={{ fontWeight: "300" }}>{contact_details[2]}</Text>
+        </Text>
+      </View>
     </View>
   );
 };
@@ -48,11 +61,17 @@ const Contact = () => {
       source={Dorji}
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text style={{marginTop:20,fontSize:20,fontWeight:'bold' }}>Contact</Text>
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "bold" }}>
+        Contact
+      </Text>
       <ScrollView style={{ marginTop: 20, width: 350 }}>
         {data.map((item, index) => {
           return (
-            <CardView key={index} company={item.company} contact_details={item.contact_details} />
+            <CardView
+              key={index}
+              company={item.company}
+              contact_details={item.contact_details}
+            />
           );
         })}
       </ScrollView>
